b9feed9dfb460a3b89e1f337bb6caa6a11e5a9b1
Stig Lindqvist <stojg.lindqvist@gmail.com>
2017-03-21 11:11:54 +1300

    add test for config file loading

---
+++ src/Config.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Elder;
+
+use SebastianBergmann\CodeCoverage\RuntimeException;
+use Symfony\Component\Yaml\Yaml;
+
+class Config
+{
+
+    /**
+     * @var string
+     */
+    protected $filePath;
+
+    /**
+     * @var array
+     */
+    protected $values;
+
+    /**
+     *
+     * @param string $configFilePath
+     */
+    public function __construct($configFilePath)
+    {
+        $this->filePath = $configFilePath;
+    }
+
+    /**
+     * load and validate configuration
+     *
+     * @throws RuntimeException
+     */
+    public function load()
+    {
+        if (!file_exists($this->filePath)) {
+            throw new \RuntimeException($this->filePath . ' file not found.');
+        }
+
+        // Load and parse all configuration.
+        $values = Yaml::parse(file_get_contents($this->filePath));
+        if(!is_array($values)) {
+            throw new \RuntimeException($this->filePath . ' file could not to be parsed as yaml');
+        }
+        $this->values = $values;
+
+        if (empty($this->values['elderUrl'])) {
+            throw new \RuntimeException($this->filePath . ' file has no "elderUrl" entry.');
+        }
+        if (empty($this->values['pages'])) {
+            throw new \RuntimeException($this->filePath . ' file has no "pages" entry.');
+        }
+        if (empty($this->values['variables'])) {
+            $this->values['variables'] = [];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getService()
+    {
+        return $this->values['elderUrl'];
+    }
+
+    /**
+     * @return array
+     */
+    public function getPages()
+    {
+        return $this->values['pages'];
+    }
+
+    /**
+     * @return array
+     */
+    public function getVariables()
+    {
+        return $this->values['variables'];
+    }
+
+}
